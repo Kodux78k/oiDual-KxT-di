cb664ba7942a426d69abf665241f6a5bac296cd7
Kodux78k <koduxlive@gmail.com>
2026-02-02 20:25:15 -0300

    Add di_KxTmain.js file

--- M0D/di_KxTmain.js
+++ M0D/di_KxTmain.js
@@ -1,4 +1,4 @@
 console.log("Infodose conectado", {"ts":1770074227260,"id":"774a71fb-1a2b-4834-b476-0a42f673afeb","meta":{"app":"generated.app"}});
 import "./modules/inline-0.js";
-import "./modules/inline-1.js";
-import "./modules/inline-2.js";
+import "./modules/KxT-di-Omega.js";
+import "./modules/KxT-di-78k.js";
