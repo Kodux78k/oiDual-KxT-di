17cc07b3115de56d50445e5591300807394be6ba
Kodux78k <koduxlive@gmail.com>
2026-02-03 00:19:05 -0300

    Add import for KxT-di-Alpha module

--- M0D/di_KxTmain.js
+++ M0D/di_KxTmain.js
@@ -1,5 +1,6 @@
 console.log("Infodose conectado", {"ts":1770074227260,"id":"774a71fb-1a2b-4834-b476-0a42f673afeb","meta":{"app":"generated.app"}});
 import "./m0ds/inline-0.js";
 import "./m0ds/KxT-di-Omega.js";
+import "./m0ds/KxT-di-Alpha.js";
 import "./m0ds/KxT-di-78k.js";
 import "https://kodux78k.github.io/oiDual-0i/0RB-0S17.js";
